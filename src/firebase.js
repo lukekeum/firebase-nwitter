b42c65804800da7b6f448360c8214ce9e157d8ae
lukekeum <lukekeum11@gmail.com>
2020-12-23 16:32:14 +0900

    1 0 Connect Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDM5i5PwNCICefQqnOIQi1aXxCDIsHU0qs',
+  authDomain: 'nwitter-48a90.firebaseapp.com',
+  projectId: 'nwitter-48a90',
+  storageBucket: 'nwitter-48a90.appspot.com',
+  messagingSenderId: '140152709452',
+  appId: '1:140152709452:web:632367cc97c07ea2fcefe9',
+};
+
+export default firebase.initializeApp(firebaseConfig);
