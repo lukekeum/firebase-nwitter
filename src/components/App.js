745ce4763b522cb011bb1a1bfe57442fcd51adb8
lukekeum <lukekeum11@gmail.com>
2020-12-23 17:04:46 +0900

    2.0 Using Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import RouterComponent from './Router';
+import { authService } from 'fbase';
 
 const App = () => {
-  return <RouterComponent />;
+  const [isLoggedIn, setIsLoggedIn] = useState(!!authService.currentUser);
+  return (
+    <>
+      <RouterComponent isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 };
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
 
-const RouterComponent = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const RouterComponent = ({ isLoggedIn = false }) => {
   return (
     <Router>
       <Switch>
